df13911e240a36ce6746b25dd6a1036cf0c0c795
andrii buga <bugandreij@gmail.com>
2025-02-03 15:05:03 +0200

    Додано моделі до додатку estimation

--- estimation/admin.py
+++ estimation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, Contract, Act
+
+
+admin.site.register(Company)
+admin.site.register(Contract)
+admin.site.register(Act)
